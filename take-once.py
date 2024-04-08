a2597214fdc35aefff188591e0559bfe5ab3eb6a
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-04-08 21:28:55 +0700

    change a bit

--- take-once.py
+++ take-once.py
@@ -7,7 +7,8 @@ now = datetime.datetime.now()
 date_time_str = now.strftime("%d%m%Y-%H%M%S")
 
 def take_photo() :
- camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+ # camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+ camera = cv2.VideoCapture(0)
  ret, frame = camera.read()
  cv2.imwrite(f"{date_time_str}.jpg", frame)
  camera.release()
