7306f4dfa8bf44ac981d0d0094cc2dbd2869f11b
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-04-08 20:52:26 +0700

    first commit

---
+++ take-once.py
@@ -0,0 +1,15 @@
+import cv2
+from matplotlib import pyplot as plt
+import datetime
+
+# just randomly take photo
+now = datetime.datetime.now()
+date_time_str = now.strftime("%d%m%Y-%H%M%S")
+
+def take_photo() :
+ camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+ ret, frame = camera.read()
+ cv2.imwrite(f"{date_time_str}.jpg", frame)
+ camera.release()
+
+take_photo()
